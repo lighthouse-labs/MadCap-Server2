2fd46fd10e181ed93fff0a10cc1503786082564c
colespen <spencerlewiscole@gmail.com>
2022-11-25 15:17:47 -0500

    setup first route for api

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,15 +1,15 @@
 const pg = require("pg");
 
-const client = new pg.Client({
+const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL || "",
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
-client
+pool
   .connect()
   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
-module.exports = client;
+module.exports = pool;
 
 
 
