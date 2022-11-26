723ce12e878d7ab242b101610f4e11b798acdfef
colespen <spencerlewiscole@gmail.com>
2022-11-26 18:29:59 -0500

    ***NPM INSTALL again for new pckg install, @mui/styles

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import './App.css';
 
-import Welcome from "./Welcome/Welcome";
+// import Welcome from "./Welcome/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      {/* Welcome is default */}
+      {/* <Welcome /> */}
+      
     </div>
   );
 }
