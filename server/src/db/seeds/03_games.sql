3c83c6b3f127b7d21e9bafdb5628c62c8c88b6f9
William Gadd <wgadd@sasktel.net>
2022-11-26 14:21:02 -0600

    Add seeds column to games table and added seed values for games and categories_sets tables

---
+++ server/src/db/seeds/03_games.sql
@@ -0,0 +1,10 @@
+INSERT INTO games (name, url, seed)
+VALUES
+('Radish Eaters', 'g45th', 14325),
+('Movie Goers', 'e45r6', 12567);
+
+INSERT INTO categories_sets (game_id, category_id)
+VALUES
+(1, 1),
+(2, 3),
+(2, 4);
