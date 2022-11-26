88e08deb9bd51186374d05295a17d8641813b747
William Gadd <wgadd@sasktel.net>
2022-11-25 20:30:02 -0600

    Add querry for a random question from the category

--- server/src/db/queries/categories.js
+++ server/src/db/queries/categories.js
@@ -7,4 +7,18 @@ const getCategories = () => {
     });
 };
 
-module.exports = { getCategories };
+const getRandomQuestion = (category_id) => {
+  return db.query(`
+  SELECT *
+  FROM subcategories
+  WHERE category_id = $1
+  ORDER BY RANDOM()
+  LIMIT 1;
+  `, [category_id]).then(data => {
+    console.log(data.rows);
+    return data.rows[0]
+  })
+}
+getRandomQuestion(1);
+
+module.exports = { getCategories, getRandomQuestion };
