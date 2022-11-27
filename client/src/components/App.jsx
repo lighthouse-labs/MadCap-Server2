867a22c54822f9eafacf9a063c1b68ce5a0735dd
colespen <spencerlewiscole@gmail.com>
2022-11-26 20:36:41 -0500

    make initial connection to categories in Lobby

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import './App.css';
 
 // import Welcome from "./Welcome/Welcome";
+import Lobby from "./Lobby/Lobby";
 
 function App() {
   return (
     <div className="App">
       {/* Welcome is default */}
       {/* <Welcome /> */}
-      
+      <Lobby />
     </div>
   );
 }
---
+++ client/src/components/Lobby/Lobby.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import SelectCategories from "./SelectCategories";
+
+import './Lobby.css';
+
+export default function Lobby(props) {
+
+  const [cats, setCats] = useState(null)
+  useEffect(() => {
+      axios.get("/api/categories")
+    .then(res => {
+      setCats(res.data.categories)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+  }, []);
+  
+  //first request is always null, fix .get() above 
+  console.log("~~~~~~ in Lobby get", cats)
+
+  return (
+    <div className="lobby-main">
+      {cats && <SelectCategories categories={cats}/>}
+    </div>
+  );
+};
