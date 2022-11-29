a9293acecb258cf1e4b590ae78e2bcdf745e54ac
William Gadd <wgadd@sasktel.net>
2022-11-28 21:20:08 -0600

    Install react-router-dom and move current files into the router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -12,10 +16,17 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 // import SocketPoC from "./SocketPoC"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
     {/* <SocketPoC /> */}
   </React.StrictMode>
 );
