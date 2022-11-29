235c9911db32673404073834a7b878fb0676b5d5
N.Kovacs <nicholasderekkovacs@gmail.com>
2022-11-29 17:05:02 -0500

    implemented splitlists

--- client/src/components/Game/AnswerList.jsx
+++ client/src/components/Game/AnswerList.jsx
@@ -1,7 +1,9 @@
 import AnswerListItem from "./AnswerListItem";
 
 export default function AnswerList(props) {
-  const answers = props.answers.map((answer) => {
+  const firstHalf = props.answers.slice(0, 12)
+  const secondHalf = props.answers.slice(12)
+  const answers1 = firstHalf.map((answer) => {
     return (
       <AnswerListItem
         key={answer.id}
@@ -11,5 +13,18 @@ export default function AnswerList(props) {
       />
     );
   });
-  return <ul>{answers}</ul>;
+  const answers2 = secondHalf.map((answer) => {
+    return (
+      <AnswerListItem
+        key={answer.id}
+        id= {answer.id}
+        letter={answer.letter}
+        captureColour={answer.captureColour}
+      />
+    );
+  });
+  return <div>
+  <ul>{answers1}</ul>
+  <ul>{answers2}</ul>
+  </div>
 }
