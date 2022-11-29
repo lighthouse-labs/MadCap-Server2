ae40831d2b3de8da50eb737280ae2892f6f535e5
N.Kovacs <nicholasderekkovacs@gmail.com>
2022-11-29 16:41:34 -0500

    shrink chat

--- client/src/components/Game/ChatListItem.jsx
+++ client/src/components/Game/ChatListItem.jsx
@@ -4,8 +4,8 @@ export default function ChatListItem(props) {
 
   return (
     <li>
-      {props.type === "chat" && <h2>{props.user}: {props.message}</h2>}
-      {props.type === "capture" && <h2>{props.user} captured {props.message}!</h2>}
+      {props.type === "chat" && <h4>{props.user}: {props.message}</h4>}
+      {props.type === "capture" && <h4>{props.user} captured {props.message}!</h4>}
     </li>
   );
 }
