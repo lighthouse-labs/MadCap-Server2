efec48e4e422e9e09893ba934ee06249ede6545c
William Gadd <wgadd@sasktel.net>
2022-11-30 11:59:50 -0600

    Change routes and queries to ajust for host column in the users table

--- server/src/db/seeds/04_users.sql
+++ server/src/db/seeds/04_users.sql
@@ -1,9 +1,9 @@
-INSERT INTO users (name, score, color, game_id, avatar_url)
+INSERT INTO users (name, score, color, game_id, avatar_url, host)
 VALUES
-('William', 10, 'yellow', 1, './avatars/avatar-temp-1.png'),
-('Spencer', 12, 'red', 1, './avatars/avatar-temp-3.png'),
-('Nicholas', 20, 'blue', 1, './avatars/avatar-temp-4.png'),
-('Harry', 14, 'yellow', 1, './avatars/avatar-temp-6.png');
+('William', 10, 'yellow', 1, './avatars/avatar-temp-1.png', true),
+('Spencer', 12, 'red', 1, './avatars/avatar-temp-3.png', false),
+('Nicholas', 20, 'blue', 1, './avatars/avatar-temp-4.png', false),
+('Harry', 14, 'yellow', 1, './avatars/avatar-temp-6.png', false);
 
 INSERT INTO users (name, color, game_id, avatar_url)
 VALUES
