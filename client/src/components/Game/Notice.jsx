e93029bf51080c48bfcbdc4e02a178087c468cc5
colespen <spencerlewiscole@gmail.com>
2022-11-29 21:22:25 -0500

    camelCase

--- client/src/components/Game/Notice.jsx
+++ client/src/components/Game/Notice.jsx
@@ -5,7 +5,7 @@ export default function Notice(props) {
   return (
     <Box className="notice" sx={{}}>
       <h2 style={{width: '270px', height: '50px',
-    'text-align': 'center'}}>{props.lastMessage}</h2>
+    textAlign: 'center'}}>{props.lastMessage}</h2>
     </Box>
   );
 }
