a21fa7cafe5e9075c8ee2e336714ef31bea44ab4
N.Kovacs <nicholasderekkovacs@gmail.com>
2022-11-29 19:21:17 -0500

    reject button

--- client/src/components/Game/AnswerListItem.jsx
+++ client/src/components/Game/AnswerListItem.jsx
@@ -7,15 +7,25 @@ export default function AnswerListItem(props) {
   let captureColour = classNames(
     props.captureColour,
     { "letter-captured": props.captureColour },
-    {"alpha2": props.id > 13 },
-    {"alpha1": props.id <= 13 },
-    {"results-phase": props.phase === "results"},
+    { alpha2: props.id > 13 },
+    { alpha1: props.id <= 13 },
+    { "results-phase": props.phase === "results" },
     {}
   );
   return (
     <li className={captureColour}>
-       {props.phase === "game" && <h2>{props.letter}</h2>}
-       {props.phase === "results" && <h2>{props.answer}</h2>}
+      {props.phase === "game" && <h2>{props.letter}</h2>}
+      {props.phase === "results" && props.answer && (
+        <h2>
+          {props.answer}
+          <button onClick={console.log("click")}>Reject!</button>
+        </h2>
+      )}
+      {props.phase === "results" && !props.answer && (
+        <h2>
+          {props.letter}
+        </h2>
+      )}
     </li>
   );
 }
