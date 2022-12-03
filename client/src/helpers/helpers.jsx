a6b83c14aaace5d1060730bd3efde63f31cf4823
N.Kovacs <nicholasderekkovacs@gmail.com>
2022-12-03 16:49:42 -0500

    cutdown

--- client/src/helpers/helpers.jsx
+++
@@ -1,9 +0,0 @@
-export const generateRandomString = function () {
-  let poschar =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < 9; i++) {
-    result += poschar.charAt(Math.floor(Math.random() * poschar.length));
-  }
-  return result;
-};
