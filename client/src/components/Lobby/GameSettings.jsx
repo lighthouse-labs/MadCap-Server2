7b55d67ee9e02c3e65935bd7ec3460b808b65826
William Gadd <wgadd@sasktel.net>
2022-12-02 19:41:06 -0600

    Disable Start button and color disabled button green

--- client/src/components/Lobby/GameSettings.jsx
+++ client/src/components/Lobby/GameSettings.jsx
@@ -55,6 +55,7 @@ export default function GameSettings(props) {
       console.error(error.message)})
   }
   const buttonText = "Start the Game"
+  console.log("Disabled", (!currentCategories)||(currentCategories.length === 0));
 
   return (
     <div className="game-settings-main">
@@ -87,6 +88,7 @@ export default function GameSettings(props) {
 
         <StartButton handleStart={handleGameStart}
           message={buttonText}
+          disabled={(!currentCategories)||(currentCategories.length === 0)}
         />
       </Box>
     </div>
--- client/src/components/Lobby/StartButton.jsx
+++ client/src/components/Lobby/StartButton.jsx
@@ -1,16 +1,26 @@
 import { Fragment } from 'react';
 import Button from '@mui/material/Button';
+import { lightGreen } from '@mui/material/colors';
 
 export default function StartButton(props) {
 
   return (
     <Fragment>
-      <Button onClick={props.handleStart} variant="contained" color="success" size="large"
+      <Button
+      onClick={props.handleStart}
+      variant="contained"
+      color="success"
+      size="large"
         sx={{
           width: '93%',
           height: 65,
-          fontSize: '21px'
+          fontSize: '21px',
+          '&.Mui-disabled': {
+           bgcolor: lightGreen[200],
+           color:lightGreen[700]
+          }
         }}
+        disabled={props.disabled}
       >{props.message}</Button>
     </Fragment>
   );
