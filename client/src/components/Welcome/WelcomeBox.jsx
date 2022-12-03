42cfc823d62eebd6bde277969c586b01e223c5cb
N.Kovacs <nicholasderekkovacs@gmail.com>
2022-12-03 13:19:02 -0500

    test

--- client/src/components/Welcome/WelcomeBox.jsx
+++ client/src/components/Welcome/WelcomeBox.jsx
@@ -39,7 +39,7 @@ export default function WelcomeBox(props) {
 
   const makeGame = () => {
    
-      axios.post("/api/games", { url })
+      axios.post("http://localhost:8001/api/games", { url })
       .then(() => (
         axios.post(`/api/games/${url}/users`, {
           name,
@@ -66,7 +66,7 @@ export default function WelcomeBox(props) {
 
   const joinGame = () => {
      
-      axios.post(`/api/games/${props.url_path}/users`, {
+      axios.post(`http://localhost:8001/api/games/${props.url_path}/users`, {
         name,
         color,
         avatar_url,
